178e49ca9bc33fc649edf5175eac0c3caffc053a
uday7223 <uday.devnur223@gmail.com>
2025-07-18 00:48:00 +0530

    added skeleton component for initial data load

---
+++ src/components/MediaSkeleton.jsx
@@ -0,0 +1,7 @@
+const MediaSkeleton = () => {
+  return (
+    <div className="w-full h-64 bg-gray-200 dark:bg-gray-700 animate-pulse rounded-lg mb-4 break-inside-avoid" />
+  );
+};
+
+export default MediaSkeleton;

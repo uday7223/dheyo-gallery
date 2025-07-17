6be3f17d2f3804d886998ca681b745adea10551d
uday7223 <uday.devnur223@gmail.com>
2025-07-18 01:23:49 +0530

    added custom loaded

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center py-6">
+      <div className="w-6 h-6 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+    </div>
+  );
+};
+
+export default Loader;
